63eaba6ea757cb9ba4ae4b312c3f43c87e249620
martinfeuillet <martin.feuillet92@gmail.com>
2022-03-31 18:12:26 +0200

    second try

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -5,19 +5,24 @@ import {
     Route,
     Routes,
     Switch,
+    useLocation,
 } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+    const location = useLocation()
     return (
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/cuisine/:type" element={<Cuisine />} />
-              <Route path='/searched/:search' element={<Searched />} />
-              <Route path='/recipe/:name' element={<Recipe />} />
-          </Routes>
+          <AnimatePresence exitBeforeEnter>
+              <Routes location={location} key={location.pathname}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/cuisine/:type" element={<Cuisine />} />
+                  <Route path='/searched/:search' element={<Searched />} />
+                  <Route path='/recipe/:name' element={<Recipe />} />
+              </Routes>
+          </AnimatePresence>
     );
 }
 
